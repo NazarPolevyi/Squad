86f7255bfb5d0ba04719398a963eb35412072a2d
Taras Kataryna <tarikkataryna1999@gmail.com>
2022-11-20 01:20:43 +0200

    Implented double check locking pattern

--- WebAdvertisements.SearchWorker/ElasticSearchHelper.cs
+++ WebAdvertisements.SearchWorker/ElasticSearchHelper.cs
@@ -7,18 +7,25 @@ namespace WebAdvertisements.SearchWorker
 {
     public static class ElasticSearchHelper
     {
+        private static readonly object _lockObj = new object();
         private static IElasticClient _client;
 
         public static IElasticClient GetInstance()
         {
             if (_client == null)
             {
-                var url = "https://search-advertapi-n5fn74stkps6pv4etvybd6z5ly.eu-central-1.es.amazonaws.com";
-                var settings = new ConnectionSettings(new Uri(url))
-                    .DefaultIndex("adverts")
-                    .DefaultTypeName("adverts")
-                    .DefaultMappingFor<AdvertType>(m => m.IdProperty(x => x.Id));
-                _client = new ElasticClient(settings);
+                lock (_lockObj)
+                {
+                    if (_client == null)
+                    {
+                        var url = "https://search-advertapi-n5fn74stkps6pv4etvybd6z5ly.eu-central-1.es.amazonaws.com";
+                        var settings = new ConnectionSettings(new Uri(url))
+                            .DefaultIndex("adverts")
+                            .DefaultTypeName("adverts")
+                            .DefaultMappingFor<AdvertType>(m => m.IdProperty(x => x.Id));
+                        _client = new ElasticClient(settings);
+                    }
+                }
             }
 
             return _client;
