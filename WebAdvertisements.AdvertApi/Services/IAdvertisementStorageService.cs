b702a290e6061fe6c69ea268bfa6710e99a347fc
Volodymyr Lysyshyn <lysyshynvova@gmail.com>
2022-11-19 23:01:37 +0200

    Add AdvertApi project to create advertisement record in DynamoDb and push message to SNS

---
+++ WebAdvertisements.AdvertApi/Services/IAdvertisementStorageService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdvertisements.AdvertApi.Models;
+
+namespace WebAdvertisements.AdvertApi.Services
+{
+    public interface IAdvertisementStorageService
+    {
+        Task<string> Add(Advertisement advertisement);
+
+        Task Confirm(ConfirmAdvertisement confirm);
+
+        Task<bool> CheckHealthAsync();
+
+        Task<Advertisement> GetByIdAsync(string id);
+
+        Task<List<Advertisement>> GetAllAsync();
+    }
+}
